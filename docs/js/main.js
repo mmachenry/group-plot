5b92bbf7e896d623992fd0a98a6a4a4af56cef2e
Mike MacHenry <mike.machenry@gmail.com>
2017-08-10 13:32:16 -0400

    fix on load issue

--- docs/js/main.js
+++ docs/js/main.js
@@ -32,7 +32,7 @@ function render_map (locations) {
   svg += "</svg>";
   
   document.getElementById("map").innerHTML = svg;
-  var panZoomTiger = svgPanZoom('#container');
+  var panZoomTiger = svgPanZoom('#world-map');
 }
 
 function render_point (location) {
